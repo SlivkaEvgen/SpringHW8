d925786b2f0ef6fe02b2bc584c78c4e07509ff5a
evgen <allrent3@gmail.com>
2021-12-06 02:07:47 +0200

    refresh 06+view2

--- target/classes/db/migration/V1__Init2_DB.sql
+++ target/classes/db/migration/V1__Init2_DB.sql
@@ -36,8 +36,11 @@ create table if not exists product
 insert into role(id, name)
 values ('1', 'ADMIN'),
        ('2', 'USER'),
-       ('3', 'PROD'),
-       ('4', 'DEV');
+       ('3', 'MODERATOR'),
+       ('4', 'PRODUCTION'),
+       ('5', 'DEVELOPER'),
+       ('6', 'OWNER'),
+       ('7', 'MY');
 
 -- create users
 insert into users (id, name, last_name, gender, email, password, role_id)
@@ -47,17 +50,19 @@ values ('1', 'USER', 'USER', 'SEX_MALE', 'yyy@oi', '123', '1'),
 
 -- create manufacturers
 insert into manufacturer (id, name)
-values ('1', 'Apple'),
-       ('2', 'Samsung'),
+values ('1', 'APPLE'),
+       ('2', 'SAMSUNG'),
        ('3', 'LG'),
-       ('4', 'Motorola');
+       ('4', 'MOTOROLA');
 
 -- create products
 insert into product (id, name, price, manufacturer_id)
-values ('1', 'Iphone', 1799.9, '1'),
-       ('2', 'lii', 890.99, '2'),
-       ('3', 'ljkbn', 1890.99, '3'),
-       ('4', 'kkn', 2290.99, '4'),
-       ('5', 'ggg', 290.99, '1');
+values ('1', 'IPHONE X', 1099.9, '1'),
+       ('2', 'TV 52', 1290.99, '2'),
+       ('3', 'MICROWAVE', 490.99, '3'),
+       ('4', 'PHONE XR', 1190.99, '4'),
+       ('5', 'IPOD', 1290.99, '1'),
+       ('6', 'MAC AIR', 2290.99, '1'),
+       ('7', 'MAC PRO', 2490.99, '1');
 
 
