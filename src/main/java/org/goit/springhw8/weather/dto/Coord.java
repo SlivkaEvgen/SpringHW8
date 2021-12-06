51269890ad45c2fda2fe41d70b1d7c4b3bea5bc5
evgen <allrent3@gmail.com>
2021-12-06 03:49:07 +0200

    refresh 06 clear

--- src/main/java/org/goit/springhw8/weather/dto/Coord.java
+++ src/main/java/org/goit/springhw8/weather/dto/Coord.java
@@ -1,12 +1,12 @@
-package org.goit.springhw8.weather.dto;
-
-import lombok.Data;
-
-@Data
-public class Coord {
-
-    public Double lon;
-
-    public Double lat;
-
-}
+//package org.goit.springhw8.weather.dto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class Coord {
+//
+//    public Double lon;
+//
+//    public Double lat;
+//
+//}
