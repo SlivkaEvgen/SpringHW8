cd99f669d462595812b178ef4491c80fd9068aba
SlivkaEvgen <allrent3@igmail.com>
2022-02-04 23:35:39 +0200

    refresh views 04

--- src/main/java/org/goit/springhw8/controller/UserController.java
+++ src/main/java/org/goit/springhw8/controller/UserController.java
@@ -7,7 +7,6 @@ import org.goit.springhw8.util.Validator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
